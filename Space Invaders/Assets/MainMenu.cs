6a8ceffb3317b5f083e3d45b9a30ae3125da7be9
Adrienne Caparaz <adcapz@gmail.com>
2019-02-12 21:40:11 -0800

    added pause menu

--- Space Invaders/Assets/MainMenu.cs
+++ Space Invaders/Assets/MainMenu.cs
@@ -7,11 +7,10 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
     public void EndGame()
     {
-        Debug.Log("quit");
         Application.Quit();
     }
 }
---
+++ Space Invaders/Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
